fb773dcbb828afd4e515389af74408e26eb64bdb
Sujit Mahato <sujitmahato678@gmail.com>
2026-09-04 22:43:39 +0530

    feat:  add server health monitoring

--- bash/server-health-check/health-check.sh
+++ bash/server-health-check/health-check.sh
@@ -4,12 +4,19 @@ echo "================================"
 echo "Starting Linux Health Check..."
 echo "================================"
 
+HOSTNAME=$(hostname)
+CURRENT_USER=$(whoami)
+UPTIME=$(uptime -p)
+IP_ADDRESS=$(hostname -I | awk '{print $1}')
+DISK_USAGE=$(df -h)
+OS_TYPE=$(cat /etc/os-release | grep "PRETTY_NAME" | cut -d '=' -f2 | tr -d '"')
+
 echo
 echo "Hostname:"
-hostname
+echo $HOSTNAME
 
 echo 
-echo "Operating System:"
+echo "Operating System: $OS_TYPE"
 cat /etc/os-release
 
 echo
@@ -17,12 +24,10 @@ echo "Kernel Version:"
 uname -r
 
 echo 
-echo "Current user"
-whoami
+echo "Current user: $CURRENT_USER"
 
 echo 
-echo "Disk Usage:"
-df -h
+echo "Disk Usage: $DISK_USAGE"
 
 echo
 echo "Memory Usage:"
@@ -33,12 +38,10 @@ echo "CPU Information:"
 lscpu
 
 echo  
-echo "Uptime:"
-uptime
+echo "Uptime: $UPTIME"
 
 echo
-echo "IP Address:"
-hostname -I
+echo "IP Address: $IP_ADDRESS"
 
 echo
 echo "Listening Ports:"
@@ -47,6 +50,13 @@ ss -lnt | head
 echo
 echo "Top 5 Memory Consuming Processes:"
 ps aux --sort=-%mem | head -n 6
+
+DISK_USAGE_PERCENT=$(df -h | awk '$NF=="/"{printf "%s", $5}')
+if [ ${DISK_USAGE_PERCENT%?} -gt 80 ]; then
+    echo "Warning: Disk usage is above 80%."
+else
+    echo "Disk usage is within acceptable limits."    
+fi
 
 echo
 echo "Health Check Completed."
